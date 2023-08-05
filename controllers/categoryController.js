05ff4cfd7a3630dffa2304f0051eccc88d6d06af
minhhuy212 <nguyenminhhuy.0938745593@gmail.com>
2023-08-05 14:50:08 +0700

    populate

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,7 +3,7 @@ const Category = require("../models/categoryModel");
 const categoryController = {
     getAllCategory: async (req, res) => {
         try {
-            const categories = await Category.find();
+            const categories = await Category.find().populate("collections");
             return res.status(200).json(categories);
         }
         catch (err) {
